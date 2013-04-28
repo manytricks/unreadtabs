fe70a4858cd6665806e6c9036d03c208251f0644
Peter Maurer <peter@manytricks.com>
2013-04-28 11:40:08 +0200

    Switched to matching via regular expression and added a duplicate check.

--- UnreadTabs.safariextension/Inject.js
+++ UnreadTabs.safariextension/Inject.js
@@ -3,24 +3,25 @@
 document.addEventListener('contextmenu', function(theEvent) {
 	var theURLs = [];
 	var theAnchors = document.getElementsByTagName('a');
-	var i;
-	var aURL;
-	var aLowercaseURL;
-	for (i = 0; i<theAnchors.length; i++) {
-		aURL = theAnchors[i].href;
-		aLowercaseURL = aURL.toLowerCase();
-		if (
-
-			// vBulletin
-			(aLowercaseURL.indexOf('goto=newpost')!==-1) ||
-			(aLowercaseURL.indexOf('-new.htm')!==-1) ||
-			(aLowercaseURL.indexOf('-new-post.htm')!==-1) ||
-
-			// Burning Board
-			(aLowercaseURL.indexOf('action=firstnew')!==-1) ||			
-
-		false) {
-			theURLs.push(aURL);
+	var theNumberOfAnchors = theAnchors.length;
+	if (theNumberOfAnchors>0) {
+		/*
+			vBulletin:
+				[&?]goto=newpost
+				-new.htm
+				-new-post.htm
+		
+			Burning Board:
+				[&?]action=firstnew
+		*/
+		var theRegularExpression = /[&?]goto=newpost|-new\.htm|-new-post\.htm|[&?]action=firstnew/i;
+		var i;
+		var aURL;
+		for (i = 0; i<theAnchors.length; i++) {
+			aURL = theAnchors[i].href;
+			if ((aURL) && (aURL.match(theRegularExpression)) && (theURLs.indexOf(aURL)===-1)) {
+				theURLs.push(aURL);
+			}
 		}
 	}
 	safari.self.tab.setContextMenuEventUserInfo(theEvent, theURLs);
